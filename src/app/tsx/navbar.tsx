7bdf8e1ecdece589369bdbe4850b71237fbce2c0
AlgolutionHK <algolutionhk@gmail.com>
2024-09-08 22:08:32 +0800

    New commit

--- src/app/tsx/navbar.tsx
+++ src/app/tsx/navbar.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 // import { Outlet, Link } from 'react-router-dom';
 // import navstyle from '@/app/css/navbar.module.css';
-import navstyle from './Navbar.module.css'
+import navstyle from '../styles/Navbar.module.css'
 // import './Navbar.css'
 // import Logo from '../../../public/logo.png';
 import '../css/w3.css';
